5e66d27785847f0b7631036e5a311d3a6454cbcd
Piotr <piotrwydmuch95@gmail.com>
2023-07-18 23:28:30 +0200

    example list rendering

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,52 @@
 import Image from 'next/image'
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('http://127.0.0.1:8000/api/advertisements/')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  interface Job {
+    title: string,
+    desc: string,
+    salary: number,
+    pub_date: string,
+  }
+
+  const listItems = data.map((job: Job, key: number) => {
+    return (
+      <li key={key} className='p-24'>
+        <p>
+          {job.title}
+        </p>
+        <p>
+          {job.salary}
+        </p>
+        <p>
+          {job.pub_date}
+        </p>
+      </li>
+    )
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <ul>
+          { listItems }
+        </ul>
+        
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
